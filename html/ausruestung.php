e0191cbb875e08a4222e6c1acc984d09fca9816a
ingressy <j.czinzoll@ingressy.de>
2025-04-23 11:28:28 +0200

    Create ausruestung.php

---
+++ html/ausruestung.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Jannik Czinzoll">
+    <meta name="keywords" content="Segeln, Ingressy, Elektrotechnik, Technik">
+    <meta name="description" content="Eine kleine Webseite oder so idk :3">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    
+    <title>Ingressy | Ausrüstung</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="bg-dark">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">ingressy</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Zuhause</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="blog.php">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="segeln.php">Segeln</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="elektrotechnik.php">Eletrotechnik</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle active" aria-current="page" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Ich?
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="eigene_dienste.php">Eigene Dienste</a></li>
+                <li><a class="dropdown-item" href="ausruestung.php">Ausrüstung</a></li>
+                <li><a class="dropdown-item" href="https://console.ingressy.de">Webinterface</a></li>
+                <li><a class="dropdown-item" href="uebermich.php">Über mich</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="impressum.php">Impressum</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="card bg-dark text-white">
+      <div class="card-body">
+        <h5>Digitale Endgeräte:</h5>
+        <h6>Laptop:</h6>
+        <p>
+          Lenovo T480<br>
+          i5-8350U | 16 GB | 500GB SSD + 128GB NVME<br>
+          mit Windows 11 und Arch Linux
+        </p>
+        <h6>PC:</h6>
+        <p>
+          CPU: Ryzen 5 3600<br>
+          GPU: 1060 mit 3GB OC<br>
+          RAM: 16GB<br>
+          Speicher: 256GB NVME + 128GB SSD + NAS<br>
+        </p>
+        <h5>IT-Infrastruktur:</h5>
+        <p>
+          drei KVMs mit insgesamt:<br>
+          6 Kern, 24GB RAM, 180GB SSD<br>
+          für Mailcow, Docker und Pterodactyl
+        </p>
+        <h6>Home Krams:</h6>
+        <p>
+          Raspberry Pi 4B 8GB mit <a href="https://www.home-assistant.io/">Home Assistant</a><br>
+          Raspberry Pi 5B 8GB für Docker Krams
+        </p>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
