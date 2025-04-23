8298efb60fb2e53da5438a112d0c5b2404971139
ingressy <j.czinzoll@ingressy.de>
2025-04-23 11:31:05 +0200

    Add software

--- html/ausruestung.php
+++ html/ausruestung.php
@@ -24,7 +24,7 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
-    
+
     <title>Ingressy | Ausrüstung</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
@@ -94,6 +94,8 @@
           Raspberry Pi 4B 8GB mit <a href="https://www.home-assistant.io/">Home Assistant</a><br>
           Raspberry Pi 5B 8GB für Docker Krams
         </p>
+        <h5>Software die ich nutze:</h5>
+        <p>Garmin Boating App | nicht als Hauptnavigationsmittel aber zum Aufzeichen</p>
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
