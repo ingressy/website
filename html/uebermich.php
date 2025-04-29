5d651c4c53a11e8bb8654f892a0d25f490d6f675
ingressy <j.czinzoll@ingressy.de>
2025-04-29 09:13:46 +0200

    Create uebermich.php

---
+++ html/uebermich.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Jannik Czinzoll">
+        <meta name="keywords" content="Segeln, Ingressy, Elektrotechnik, Technik">
+        <meta name="description" content="Eine kleine Webseite oder so idk :3">
+    
+        <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/favicon/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+
+        <title>Ingressy | Über mich</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+    <body class="bg-dark">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">ingressy</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Zuhause</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="blog.php">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="segeln.php">Segeln</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="elektrotechnik.php">Eletrotechnik</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle active" aria-current="page" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Ich?
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="eigene_dienste.php">Eigene Dienste</a></li>
+                                <li><a class="dropdown-item" href="ausruestung.php">Ausrüstung</a></li>
+                                <li><a class="dropdown-item" href="https://console.ingressy.de">Webinterface</a></li>
+                                <li><a class="dropdown-item" href="uebermich.php">Über mich</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="impressum.php">Impressum</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="card bg-dark text-white">
+            <div class="card-body">
+                <p>Du willst was über mich wissen? Hm, oke ^^</p>
+                <p>
+                    <strong>Wer bist du?</strong> <br>
+                    <i>Ich glaube, ich bin ein Mensch ... vielleicht bin ich aber auch eine Katze ~meow</i><br>
+                    <strong>Welches Geschlecht hast du?</strong> <br>
+                    <i>Ich bin tatsächlich Nicht Binär. Und nutze eigentlich alle Pronomen außer er/ihm!</i><br>
+                    <strong>Wie alt bist du?</strong> <br>
+                    <i>Ich bin mittlerweile 18 Jahre alt</i><br>
+                    <strong>Was ist deine Lieblingstiere?</strong><br>
+                    <i>Katzen, Fuchse und Hasen i guess</i><br>
+                    <strong>Was sind deine Lieblingssportarten?</strong><br>
+                    <i>Segeln und Schwimmen <s>Sport ist scheiße!</s></i><br>
+                    <strong>Was machst du beruflich?</strong><br>
+                    <i>Im Moment noch gar nix. Gehe auf ein Berufliches Gymnasium und habe als Leistungskurs Informatik und wahrscheinlich Deutsch</i>
+                    <strong>Was interessiert dich so?</strong><br>
+                    <i>Im kurzen gesagt: Segelschiff (im besondern Traditionssegler), 3D Druck, Elektrotechnik und Informatik</i>
+                </p>
+                <p>
+                    <strong>Was für Schiffe segelst du?</strong> <br>
+                    <i>Jolle: Pirat, 420er; Schiff: 25m Traditionssegler und 8,20m Segelschiff </i><br>
+                    <strong>Wie viele Seemeilen bist du schon gesegelt?</strong> <br>
+                    <i>über 809sm (1498 km) seit 2023</i><br>
+                    <strong>Was für Segelscheine hast du?</strong><br>
+                    <i>Im Moment Sportbootführerschein See Motor und Sportbootführerschein Binnen Motor und Segeln</i><br>
+                    <strong>Was war dein schönster Törn?</strong><br>
+                    <i>Naja ob es schön war, muss jeder selber entscheiden: von Lauwersoog, NL -> Bremen Vegesack, DE; mit 7 Crewmitgliedern in 26h, über die Nordfriesischen Inseln hinweg</i><br>
+                    <strong>Welche Strecke bist du am Häufigsten bis jetzt gefahren?</strong><br>
+                    <i>von Bremen -> Elsfleth; von Laboe -> NOK und Schiffahrtsweg Elbe-Weser -> Bremen und Bremerhaven -> Kaiser balje -> Wilhelmshaven </i><br>
+                    <strong>Hast du einen "Rank" auf einen Segelschiff?</strong><br>
+                    <i>Ich bin "2. Boots in Ausbildung" auf einen Plattbodenschiff.</i>
+                </p>
+                <p>
+                    <strong>Was für ein 3D Drucker hast du?</strong> <br>
+                    <i>Im Moment habe ich ein BambuLab A1 mit AMS Lite mit knapp 500h Betriebstunden und bin komplett zufrieden damit</i><br>
+                </p>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>
